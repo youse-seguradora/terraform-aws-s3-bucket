e6eb8073126564a5857b21e48246de7b09a57526
Amanda Soares de Carvalho <amanda.carvalho@youse.com.br>
2020-04-13 17:02:46 -0300

    add localstack

--- examples/s3-replication/main.tf
+++ examples/s3-replication/main.tf
@@ -6,13 +6,71 @@ locals {
 }
 
 provider "aws" {
-  region = local.origin_region
+  access_key                  = "mock_access_key"
+  region                      = local.origin_region
+  s3_force_path_style         = true
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+
+  endpoints {
+    apigateway     = "http://localstack-s3:4567"
+    cloudformation = "http://localstack-s3:4581"
+    cloudwatch     = "http://localstack-s3:4582"
+    dynamodb       = "http://localstack-s3:4569"
+    es             = "http://localstack-s3:4578"
+    firehose       = "http://localstack-s3:4573"
+    iam            = "http://localstack-s3:4593"
+    kinesis        = "http://localstack-s3:4568"
+    kms            = "http://localstack-s3:4599"
+    lambda         = "http://localstack-s3:4574"
+    route53        = "http://localstack-s3:4580"
+    redshift       = "http://localstack-s3:4577"
+    s3             = "http://localstack-s3:4572"
+    secretsmanager = "http://localstack-s3:4584"
+    ses            = "http://localstack-s3:4579"
+    sns            = "http://localstack-s3:4575"
+    sqs            = "http://localstack-s3:4576"
+    ssm            = "http://localstack-s3:4583"
+    stepfunctions  = "http://localstack-s3:4585"
+    sts            = "http://localstack-s3:4592"
+  }
 }
 
 provider "aws" {
-  region = local.replica_region
+  access_key                  = "mock_access_key"
+  region                      = local.replica_region
+  s3_force_path_style         = true
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  alias                       = "replica"
 
-  alias = "replica"
+  endpoints {
+    apigateway     = "http://localstack-s3:4567"
+    cloudformation = "http://localstack-s3:4581"
+    cloudwatch     = "http://localstack-s3:4582"
+    dynamodb       = "http://localstack-s3:4569"
+    es             = "http://localstack-s3:4578"
+    firehose       = "http://localstack-s3:4573"
+    iam            = "http://localstack-s3:4593"
+    kinesis        = "http://localstack-s3:4568"
+    kms            = "http://localstack-s3:4599"
+    lambda         = "http://localstack-s3:4574"
+    route53        = "http://localstack-s3:4580"
+    redshift       = "http://localstack-s3:4577"
+    s3             = "http://localstack-s3:4572"
+    secretsmanager = "http://localstack-s3:4584"
+    ses            = "http://localstack-s3:4579"
+    sns            = "http://localstack-s3:4575"
+    sqs            = "http://localstack-s3:4576"
+    ssm            = "http://localstack-s3:4583"
+    stepfunctions  = "http://localstack-s3:4585"
+    sts            = "http://localstack-s3:4592"
+  }
 }
 
 data "aws_caller_identity" "current" {}
@@ -22,7 +80,7 @@ resource "random_pet" "this" {
 }
 
 resource "aws_kms_key" "replica" {
-  provider = "aws.replica"
+  provider = aws.replica
 
   description             = "S3 bucket replication KMS key"
   deletion_window_in_days = 7
@@ -32,7 +90,7 @@ module "replica_bucket" {
   source = "../../"
 
   providers = {
-    aws = "aws.replica"
+    aws = aws.replica
   }
 
   bucket = local.destination_bucket_name
