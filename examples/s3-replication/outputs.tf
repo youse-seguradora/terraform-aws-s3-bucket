3f10e6c7ffc0c380a33247d96eba3d7e46cf031c
Amanda Soares de Carvalho <amanda.carvalho@youse.com.br>
2020-04-16 19:20:40 -0300

    create output name to s3 bucket replica

--- examples/s3-replication/outputs.tf
+++ examples/s3-replication/outputs.tf
@@ -3,6 +3,11 @@ output "this_s3_bucket_id" {
   value       = module.s3_bucket.this_s3_bucket_id
 }
 
+output "this_replica_s3_bucket_id" {
+  description = "The name of the bucket."
+  value       = module.replica_bucket.this_s3_bucket_id
+}
+
 output "this_s3_bucket_arn" {
   description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
   value       = module.s3_bucket.this_s3_bucket_arn
